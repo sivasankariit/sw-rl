e4e1821b804443600a7e8c06d0f0b601e401c787
Vimalkumar <jvimal@google.com>
2012-08-24 09:51:33 -0700

    More RR sizes in RR tests

--- tests/run.sh
+++ tests/run.sh
@@ -5,18 +5,24 @@ time=20
 ns=0
 
 mkdir -p $dir
-for nrr in 512; do
+for nrr in 512 1024; do
+for rrsize in 1 10000 1000000; do
 for rl in newrl htb none; do
-    python netperf.py --nrr $nrr --exptid rl-$rl -t $time \
+    exptid=rl-$rl-rrsize-$rrsize
+    python netperf.py --nrr $nrr \
+        --exptid $exptid \
+        -t $time \
         --ns $ns \
-        --rl $rl
+        --rl $rl \
+        --rrsize $rrsize
 
-    mv rl-$rl.tar.gz $dir/
+    mv $exptid.tar.gz $dir/
 
     pushd $dir;
-    tar xf rl-$rl.tar.gz
-    python ../plot.py --rr rl-$rl/* -o $rl.png --ymin 0.9
+    tar xf $exptid.tar.gz
+    python ../plot.py --rr $exptid/* -o $exptid.png --ymin 0.9
     popd;
+done;
 done
 done
 
