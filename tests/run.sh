40dc4d37aa1551276620d08527b806e796b4cab2
Vimalkumar <jvimal@google.com>
2012-08-27 13:50:33 -0700

    Updates to latency test

--- tests/run.sh
+++ tests/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 dir=`date +%b%d-%H:%M`
-time=20
+time=30
 ns=0
 
 mkdir -p $dir
-for nrr in 512 1024; do
-for rrsize in 1 10000 1000000; do
-for rl in newrl htb none; do
+for nrr in 512; do
+for rrsize in 1; do
+for rl in htb newrl none; do
     exptid=rl-$rl-rrsize-$rrsize
     python netperf.py --nrr $nrr \
         --exptid $exptid \
